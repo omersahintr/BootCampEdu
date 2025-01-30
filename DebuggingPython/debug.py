52bccd88187219cb2b2b763498d1f02a67851590
Omer Sahin <bilgi@omersahin.com.tr>
2025-01-30 10:09:03 +0300

    numPyFunctions.py has added

--- DebuggingPython/debug.py
+++ DebuggingPython/debug.py
@@ -1,2 +1,21 @@
-import os as o
+class MyClass:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+    def increaseAge(self):
+        self.age += 1
+
+    def decrease(self):
+        if self.age > 0:
+            self.age -= 1
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+David = MyClass("David",6)
+David.decrease()
+David.decrease()
+
 
