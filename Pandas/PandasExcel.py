b8a3ac8833e500acc78be2a1a40c24a935e0065a
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-07 16:12:47 +0300

    excel py updated and push

--- Pandas/PandasExcel.py
+++ Pandas/PandasExcel.py
@@ -4,3 +4,48 @@ import openpyxl
 
 data_frame = pd.read_excel("sales.xlsx")
 print(data_frame)
+
+# 1) How many rows of data are there in total?
+
+count_rows = data_frame["Employee Name"].count()
+print(f"Total Rows Count: {count_rows}")
+                # Total Rows Count: 100
+
+# 2) What is the average salary this company pays?
+
+averaje_salary = data_frame["Salary"].mean()
+print(f"Average Salary: {averaje_salary:.2f}")
+                # Average Salary: 725.84
+
+# 3) How does the average salary compare across departments at this company?
+
+averaje_salary_of_department = data_frame.groupby("Department")["Salary"].mean()
+print(f"Average Salary of Department: \n{averaje_salary_of_department.round(2)}")
+"""             Average Salary of Department:
+                Department
+                Finance                 805.41
+                HR                      640.94
+                Marketing               707.00
+                Sales                   656.67
+                Software Development    826.41
+                Name: Salary, dtype: float64
+"""
+
+# #4) How does the average salary compare by title (senior - junior) at this company?
+averaje_salary_of_title = data_frame.groupby("Title")["Salary"].mean()
+print(f"Average Salary of Title: \n{averaje_salary_of_title.round(2)}")
+"""         Average Salary of Title: 
+                Title
+                C-level       1058.33
+                Junior         440.62
+                Mid            641.45
+                Mid-Senior     725.50
+                Senior         867.78
+                Name: Salary, dtype: float64
+"""
+
+
+
+
+
+
