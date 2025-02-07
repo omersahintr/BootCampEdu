2bc52b9128a1c899cad13961ac93cf4fcbfb9161
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-07 21:36:50 +0300

    new

--- MatPlot/matplotFirst.py
+++ MatPlot/matplotFirst.py
@@ -1,13 +1,13 @@
-import matplotlib as mp
+"""import matplotlib as mp
 import matplotlib.pyplot as plt 
 import numpy as np
 import pandas as pd
+
+"""
 import os
 
 
-
-
-f = open("afad-veri.txt", "r")
+f = open('afad-veri.txt', 'r')
 print(f.read(5))
 
 """depremFrame = pd.dataFrame(open('afad-veri.txt', 'r'))
