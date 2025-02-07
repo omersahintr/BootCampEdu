8fc690e7628964a33ca6c3a6a923aee767064527
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-08 01:35:20 +0300

    matplot added

--- MatPlot/matplotFirst.py
+++ MatPlot/matplotFirst.py
@@ -1,16 +1,21 @@
-"""import matplotlib as mp
+import matplotlib as mp
 import matplotlib.pyplot as plt 
 import numpy as np
 import pandas as pd
+import requests as rq
+from io import StringIO
 
-"""
-import os
+start_date = "2025-02-06%2010:00:00"
+end_date = "2025-02-07%2010:00:00"
+max_mag = 9.0
+min_mag = 2.0
+url = f"https://servisnet.afad.gov.tr/apigateway/deprem/apiv2/event/filter?minmag={min_mag}&maxmag={max_mag}&start={start_date}&end={end_date}&format=csv"
+
+s = rq.get(url).text
+c = pd.read_csv(StringIO(s))
+print(c[["Date","Rms","Location","Magnitude"]])
 
 
-f = open('afad-veri.txt', 'r')
-print(f.read(5))
 
-"""depremFrame = pd.dataFrame(open('afad-veri.txt', 'r'))
 
-print(depremFrame.head())
-"""
+
