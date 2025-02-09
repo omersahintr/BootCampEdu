907661a6933c89c7a387a24106aaa07bb6daff08
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-10 02:15:25 +0300

    same updates added

--- MatPlot/matplotFirst.py
+++ MatPlot/matplotFirst.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-"""age = [25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45]
+age = [25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45]
 height = [168, 134, 210, 174, 176, 188, 180, 150, 184, 121, 188]
 
 np_age = np.array(age) # convert list to numpy array
@@ -14,12 +14,12 @@ plt.xlabel('Age') # x-axis label
 plt.ylabel('Height') # y-axis label
 plt.title('Age vs Height') # title of the graph
 plt.plot(np_age, np_height,"g") # g is for green color
-plt.show() # display the graph"""
+plt.show() # display the graph
 
 
 
 
-"""new_numpay1 = np.linspace(0, 10, 20) # 20 numbers between 0 and 10
+new_numpay1 = np.linspace(0, 10, 20) # 20 numbers between 0 and 10
 new_numpay2 = new_numpay1 ** 2 # square of each number in new_numpay1
 print(new_numpay2) 
 
@@ -27,7 +27,7 @@ plt.subplot(1, 2, 1) # 1 row, 2 columns, 1st position
 plt.plot(new_numpay1, new_numpay2, 'r*-') # r is for red color + is for plus sign
 plt.subplot(1, 2, 2) # 1 row, 2 columns, 2nd position
 plt.plot(new_numpay2, new_numpay1, 'g+-') # g is for green color -- is for dash line
-plt.show() # display the graph"""
+plt.show() # display the graph
 
 
 
