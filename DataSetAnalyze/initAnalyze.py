c62ee74018be0b6f86280bf50a95bdc2df415c8c
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-12 12:05:12 +0300

    first commit added

---
+++ DataSetAnalyze/initAnalyze.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib as mpl
+
+data_file = 'train.csv'
+
+dataset = pd.read_csv(data_file)
+
+print(dataset.head())
