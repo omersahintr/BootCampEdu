4d4d0976653914589a4cb48ee7d8e933ffbafade
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-18 23:27:42 +0300

    file name has changed

--- NetProject/requestPostPutPatchDelete.py
+++ NetProject/requestPostPutPatchDelete.py
@@ -8,14 +8,14 @@ print(response.json())
 
 
 
-## POST Example
+## POST Example:
 myToDo = {
     "userId": 2,
     "title": "try first post",
     "completed": False
 }
 MyPostUrl = "https://jsonplaceholder.typicode.com/todos"
-post_response = req.post(MyPostUrl, myToDo)
+post_response = req.post(MyPostUrl, json=myToDo)
 print(post_response.json()) 
       #{'userId': '2', 'title': 'try first post', 'completed': 'False', 'id': 201}
 
