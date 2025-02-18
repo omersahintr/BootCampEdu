8c7ff64dfe2d778176817703514a6ae8ee7d16e6
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-18 22:37:36 +0300

    asyncio object added

---
+++ Threading/asyncio-threading.py
@@ -0,0 +1,29 @@
+import threading
+import requests as req
+import time
+import asyncio
+import aiohttp
+
+
+async def get_web_async_wrapper(urls):
+    start_time = time.time()
+    jason_data = []
+    
+    async with aiohttp.ClientSession() as session:
+        for url in urls:
+            async with session.get(url) as response:
+                jason_data.append(await response.json())
+
+
+    end_time = time.time()
+    dif_time = end_time - start_time
+    print(f"Time: {dif_time}")
+    return jason_data
+
+
+urls = ['https://postman-echo.com/delay/2'] * 8 # 8 times request delay 2 seconds
+
+asyncio.run(get_web_async_wrapper(urls))
+
+
+
