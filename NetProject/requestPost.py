9219aff12b546f151a6f1bcf24661abe6489abf6
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-18 23:10:16 +0300

    changed file names

---
+++ NetProject/requestPost.py
@@ -0,0 +1,25 @@
+import requests as req
+
+
+## GET Example:
+myGetUrl = "https://jsonplaceholder.typicode.com/todos"
+response = req.get(myGetUrl)
+print(response.json())
+
+
+
+## POST Example
+myToDo = {
+    "userId": 2,
+    "title": "try first post",
+    "completed": False
+}
+MyPostUrl = "https://jsonplaceholder.typicode.com/todos"
+post_response = req.post(MyPostUrl, myToDo)
+print(post_response.json()) 
+      #{'userId': '2', 'title': 'try first post', 'completed': 'False', 'id': 201}
+
+
+
+#PUT Example:
+
