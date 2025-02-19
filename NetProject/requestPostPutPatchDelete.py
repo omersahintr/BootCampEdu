795aef81345252e2b68229e026379c761c0105d0
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-19 13:11:31 +0300

    post, put, patch, get and delete metod using this class.

--- NetProject/requestPostPutPatchDelete.py
+++ NetProject/requestPostPutPatchDelete.py
@@ -22,4 +22,31 @@ print(post_response.json())
 
 
 #PUT Example:
+PutUrl = "https://jsonplaceholder.typicode.com/todos/2"
+MyPut = {
+    "userId": 2,
+    "title": "try second put",
+    "completed": False
+}
+MyPutUrl = req.put(PutUrl, json=MyPut)
+print(MyPutUrl.json())
+    #{'userId': 2, 'title': 'try second put', 'completed': False, 'id': 2}
+
+
+
+ # PATCH Examples:
+PatchUrl = "https://jsonplaceholder.typicode.com/todos/2"
+MyPatch = {
+    "title": "try last patch"
+}   
+MyPatchUrl = req.patch(PatchUrl, json=MyPatch)
+print(MyPatchUrl.json())
+    #{'userId': 2, 'title': 'try last patch', 'completed': False, 'id': 2}
+
+
+# DELETE Example:
+deleteUrl = "https://jsonplaceholder.typicode.com/todos/2"
+myDelete = req.delete(deleteUrl)
+print(myDelete.json())
+    #{}  #Empty because the data has been deleted. only--> {}
 
