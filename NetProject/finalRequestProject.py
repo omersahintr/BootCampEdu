dc26f7d8806f54b985c59d474be380b97fefe666
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-22 17:15:56 +0300

    place localiser has added

--- NetProject/finalRequestProject.py
+++ NetProject/finalRequestProject.py
@@ -10,36 +10,68 @@ window.minsize(width=900, height=1000)
 ###########################################
 
 # Labels creating:
-labelAddress = tk.Label(text="Site Address:")
-labelAddress.config(fg="black", font=("Verdana",12,"bold"))
+labelAddress = tk.Label(text="Site Address:", fg="black", font=("Verdana",12,"bold"))
 
-labelStatus = tk.Label(text="Status: Ready",  font=("Verdana",18,"bold"))
-labelStatus.config(fg="green")
+labelStatus = tk.Label(text="Status: Ready",  font=("Verdana",18,"bold"), fg="green")
+
+labelH1Count = tk.Label(text="H1 Count:", fg="black", font=("Verdana",14,"underline"))
+
+labelH1 = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
+
+labelH2Count = tk.Label(text="H2 Count:", fg="black", font=("Verdana",14,"underline"))
+
+labelH2 = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
+
+labelH3Count = tk.Label(text="H3 Count:", fg="black", font=("Verdana",14,"underline"))
+
+labelH3 = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
+
+labelH4Count = tk.Label(text="H4 Count:", fg="black", font=("Verdana",14,"underline"))
+
+labelH4 = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
+
+labelImageCount = tk.Label(text="Images:", fg="black", font=("Verdana",14,"underline"))
+
+labelImage = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
+
+labelLinkCount = tk.Label(text="Links:", fg="black", font=("Verdana",14,"underline"))
+
+labelLink = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
 
 
-
-
-# Entery Objects Creating for Window
-enteryAddress = tk.Entry(font=("Verdana",12,"normal"), width=40)
+# Entry Objects Creating for Window
+entryAddress = tk.Entry(font=("Verdana",12,"normal"), width=40)
 
 
 
 # Button Objects Creating for Window
-sendButton = tk.Button(text="Start Scan", font=("Arial",14,"bold"), command=lambda:heading_counter(enteryAddress.get()))
+sendButton = tk.Button(text="Start Scan", font=("Arial",14,"bold"), command=lambda:heading_counter(entryAddress.get()))
 
 
 ### PLACE LOCALISER ###
 #labelStatus.grid(row=1,column=0)
-labelStatus.place(x=400, y=10)
+labelStatus.place(x=200, y=10)
 labelAddress.place(x=10, y=40)
-enteryAddress.place(x=110, y=40); enteryAddress.focus()
+entryAddress.place(x=110, y=40); entryAddress.focus()
 sendButton.place(x=450, y=36)
+
+labelH1Count.place(x=10,y=100)
+labelH1.place(x=10,y=120)
+labelH2Count.place(x=100,y=100)
+labelH2.place(x=100,y=120)
+labelH3Count.place(x=200,y=100)
+labelH3.place(x=200,y=120)
+labelH4Count.place(x=300,y=100)
+labelH4.place(x=300,y=120)
 
 
 
 
 def heading_counter(url):
-    labelStatus.config(text=enteryAddress.get())
+    if url:
+        labelStatus.config(text=f"Scanning to: {entryAddress.get()}", fg="green")
+    else:
+        labelStatus.config(text="Status: Please enter an address", fg="red")
 
 
 
