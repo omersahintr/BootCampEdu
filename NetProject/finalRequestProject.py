a0d615763e88349bc8f9aee602f0de83dd84d8ef
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-22 17:44:02 +0300

    saved from history.

--- NetProject/finalRequestProject.py
+++ NetProject/finalRequestProject.py
@@ -14,29 +14,31 @@ labelAddress = tk.Label(text="Site Address:", fg="black", font=("Verdana",12,"bo
 
 labelStatus = tk.Label(text="Status: Ready",  font=("Verdana",18,"bold"), fg="green")
 
-labelH1Count = tk.Label(text="H1 Count:", fg="black", font=("Verdana",14,"underline"))
+labelH1Count = tk.Label(text="H1", fg="black", bg="#02df5d", font=("Verdana",14,"underline"))
 
 labelH1 = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
 
-labelH2Count = tk.Label(text="H2 Count:", fg="black", font=("Verdana",14,"underline"))
+labelH2Count = tk.Label(text="H2", fg="black", bg="#95df02", font=("Verdana",14,"underline"))
 
 labelH2 = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
 
-labelH3Count = tk.Label(text="H3 Count:", fg="black", font=("Verdana",14,"underline"))
+labelH3Count = tk.Label(text="H3", fg="black", bg="#daf205", font=("Verdana",14,"underline"))
 
 labelH3 = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
 
-labelH4Count = tk.Label(text="H4 Count:", fg="black", font=("Verdana",14,"underline"))
+labelH4Count = tk.Label(text="H4", fg="black", bg="#f2d405", font=("Verdana",14,"underline"))
 
 labelH4 = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
 
-labelImageCount = tk.Label(text="Images:", fg="black", font=("Verdana",14,"underline"))
+labelImageCount = tk.Label(text="Images", bg="#05f2d4", fg="black", font=("Verdana",14,"underline"))
 
 labelImage = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
 
-labelLinkCount = tk.Label(text="Links:", fg="black", font=("Verdana",14,"underline"))
+labelLinkCount = tk.Label(text="Links", bg="#0591f2", fg="black", font=("Verdana",14,"underline"))
 
 labelLink = tk.Label(text="-", fg="black", font=("Verdana",16,"bold"))
+
+labelResults = tk.Label(text="RESULTS:", fg="black", font=("Verdana",16,"bold"))
 
 
 # Entry Objects Creating for Window
@@ -55,15 +57,20 @@ labelAddress.place(x=10, y=40)
 entryAddress.place(x=110, y=40); entryAddress.focus()
 sendButton.place(x=450, y=36)
 
-labelH1Count.place(x=10,y=100)
-labelH1.place(x=10,y=120)
-labelH2Count.place(x=100,y=100)
-labelH2.place(x=100,y=120)
-labelH3Count.place(x=200,y=100)
-labelH3.place(x=200,y=120)
-labelH4Count.place(x=300,y=100)
-labelH4.place(x=300,y=120)
-
+y1=140; y2=165; x1=10; x2=15
+labelResults.place(x=x1,y=100)
+labelH1Count.place(x=x1,y=y1)
+labelH1.place(x=x2,y=y2)
+labelH2Count.place(x=x1+100,y=y1)
+labelH2.place(x=x2+100,y=y2)
+labelH3Count.place(x=x1+200,y=y1)
+labelH3.place(x=x2+200,y=y2)
+labelH4Count.place(x=x1+300,y=y1)
+labelH4.place(x=x2+300,y=y2)
+labelImageCount.place(x=x1+400,y=y1)
+labelImage.place(x=x2+400,y=y2)
+labelLinkCount.place(x=x1+500,y=y1)
+labelLink.place(x=x2+500,y=y2)
 
 
 
