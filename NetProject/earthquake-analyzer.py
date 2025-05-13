1006e53ab088cc5f7c12d9cfe8919b9df7d22703
Omer Sahin <bilgi@omersahin.com.tr>
2025-05-13 23:47:43 +0300

    locator changed province

--- NetProject/earthquake-analyzer.py
+++ NetProject/earthquake-analyzer.py
@@ -21,27 +21,29 @@ end_date_hour = str(now.hour)
 end_date_minute = str(now.minute)
 end_date_second = str(now.second)
 
-locator = "Marmara Denizi"
-mag = 3.0
+locator = "İstanbul"
+mag = 4.0
 
 # RESPONSE FUNCTION:
 def response_url():
     i=0
     data_frame = pd.DataFrame()
     dict = {}
-    url = f"https://servisnet.afad.gov.tr/apigateway/deprem/apiv2/event/filter?start={start_date_year}-{start_date_month}-{start_date_day}%20{start_date_hour}:{start_date_minute}:{start_date_second}&end={end_date_year}-{end_date_month}-{end_date_day}%20{end_date_hour}:{end_date_minute}:{end_date_second}" #for live data
+    url = f"https://servisnet.afad.gov.tr/apigateway/deprem/apiv2/event/filter?start={start_date_year}-{start_date_month}-{start_date_day}%20{start_date_hour}:{start_date_minute}:{start_date_second}&end={end_date_year}-{end_date_month}-{end_date_day}%20{end_date_hour}:{end_date_minute}:{end_date_second}" #for afad live data
     #print(req.get(url).status_code)
+
+    plt.autoscale=True
     plt.title(f"{locator} Depremler-{start_date_year}")
     plt.xlabel("Date")
-    plt.ylabel("Magnitude")
+    plt.ylabel("Mw")
     response = req.get(url)
     if response.status_code == 200:   
         for query in response.json():
-            if query["location"] != locator and float(query["magnitude"])>mag:
+            if query["province"] == locator and float(query["magnitude"])>mag:
                 i+=1
                 plt.scatter((query["date"]), float(query["magnitude"]), color="red")
                 #plt.plot((query["date"])[0:10], float(query["magnitude"]), color="red")
-                print(f"{i} - {query["date"][0:10]}-{query["location"]}-{query["magnitude"]}--{query["depth"]}")
+                print(f"{i} - {query["date"][0:16]}-{query["location"]}-{query["magnitude"]}--{query["depth"]}")
         plt.show()
 
 response_url()
